08b4e587802de4fa7c3ef3efcce8d2f0ab5136dd
Maurice Prosper <maurice.prosper@ttu.edu>
2016-01-06 01:47:39 -0600

    Clean after testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function (grunt) {
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'php_config', 'nodeunit']);
+    grunt.registerTask('test', ['clean', 'php_config', 'nodeunit', 'clean']);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['jshint', 'test']);
